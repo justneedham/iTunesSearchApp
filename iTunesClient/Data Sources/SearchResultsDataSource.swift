2a8a50f9ccfacd8f8d2f0bcf514fb29844522814
Justin Needham <justin.needham@alumnimail.pepperdine.edu>
2019-03-17 10:53:53 -0600

    add table view with stub

---
+++ iTunesClient/Data Sources/SearchResultsDataSource.swift
@@ -0,0 +1,38 @@
+//
+//  SearchResultsDataSource.swift
+//  iTunesClient
+//
+//  Created by Justin Needham on 2019-03-17.
+//  Copyright © 2019 Treehouse Island. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultsDataSource: NSObject, UITableViewDataSource {
+
+    private var data = [Artist]()
+
+    override init() {
+        super.init()
+    }
+
+    func update(with artists: [Artist]) {
+        data = artists
+    }
+
+    // MARK: - Data Source
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "resultCell", for: indexPath)
+        let artist = data[indexPath.row]
+        cell.textLabel?.text = artist.name
+        return cell
+    }
+}
---
+++ iTunesClient/SearchResultsController.swift
@@ -0,0 +1,104 @@
+//
+//  SearchResultsController.swift
+//  iTunesClient
+//
+//  Created by Justin Needham on 2019-03-17.
+//  Copyright © 2019 Treehouse Island. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultsController: UITableViewController {
+
+    let searchController = UISearchController(searchResultsController: nil)
+    let dataSource = SearchResultsDataSource()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+         self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(SearchResultsController.dismissSearchResultsController))
+        tableView.tableHeaderView = searchController.searchBar
+        searchController.dimsBackgroundDuringPresentation = false
+        searchController.searchResultsUpdater = self
+
+        tableView.dataSource = dataSource
+    }
+
+    func dismissSearchResultsController() {
+        self.dismiss(animated: true, completion: nil)
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension SearchResultsController: UISearchResultsUpdating {
+    public func updateSearchResults(for searchController: UISearchController) {
+        dataSource.update(with: [Stub.artist])
+        tableView.reloadData()
+    }
+}
