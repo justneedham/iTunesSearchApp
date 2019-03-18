1e60d19a9342f2bcf3131382d688424f2dac0bf1
Justin Needham <justin.needham@alumnimail.pepperdine.edu>
2019-03-17 18:33:56 -0600

    add search endpoint

--- iTunesClient/ViewController.swift
+++ iTunesClient/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let endpoint = iTunes.search(term: "taylor swift", media: .music)
+        print(endpoint.request)
     }
 
     override func didReceiveMemoryWarning() {
---
+++ iTunesClient/iTuneMedia.swift
@@ -0,0 +1,36 @@
+//
+// Created by Justin Needham on 2019-03-17.
+// Copyright (c) 2019 Treehouse Island. All rights reserved.
+//
+
+import Foundation
+
+enum ItunesMedia {
+    case movie
+    case podcast
+    case music
+    case musicVideo
+    case audiobook
+    case shortFilm
+    case tvShow
+    case software
+    case ebook
+    case all
+}
+
+extension ItunesMedia: CustomStringConvertible {
+    var description: String {
+        switch self {
+        case .movie: return "movie"
+        case .podcast: return "podcast"
+        case .music: return "music"
+        case .musicVideo: return "musicVideo"
+        case .audiobook: return "audiobook"
+        case .shortFilm: return "shortFilm"
+        case .tvShow: return "tvShow"
+        case .software: return "software"
+        case .ebook: return "ebook"
+        case .all: return "all"
+        }
+    }
+}
