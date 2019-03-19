3c30f65f84bf3fc222b2bb96e3f9976e8f21d07a
Justin Needham <justin.needham@alumnimail.pepperdine.edu>
2019-03-18 18:21:48 -0600

    add album search API

--- iTunesClient/Data Sources/AlbumListDataSource.swift
+++ iTunesClient/Data Sources/AlbumListDataSource.swift
@@ -7,7 +7,7 @@ import UIKit
 
 class AlbumListDataSource: NSObject, UITableViewDataSource {
 
-    private let albums: [Album]
+    private var albums: [Album]
 
     init(albums: [Album]) {
         self.albums = albums
@@ -35,5 +35,9 @@ class AlbumListDataSource: NSObject, UITableViewDataSource {
 
     func album(at indexPath: IndexPath) -> Album {
         return albums[indexPath.row]
+    }
+
+    func update(with albums: [Album]) {
+        self.albums = albums
     }
 }
